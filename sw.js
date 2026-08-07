3e7287355f739474550dd951211105e865db0b9a
IsraelToledano <86730653+IsraelToledano@users.noreply.github.com>
2026-08-07 12:34:49 +0300

    Bump cache to v9 for image and reorder fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "gym-buddy-v6";
+const CACHE = "gym-buddy-v9";
 const IMG_CACHE = "gym-buddy-exercise-img-v1";
 const IMG_HOST = "raw.githubusercontent.com";
 const ASSETS = ["./", "./index.html", "./manifest.json"];
