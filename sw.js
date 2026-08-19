438b5ad0c753b56c9d2c9e0a7f2084b200c72d0b
IsraelToledano <86730653+IsraelToledano@users.noreply.github.com>
2026-08-19 19:06:52 +0300

    Switch weekly volume tracking to rolling 7-day window v24

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "gym-buddy-v23";
+const CACHE = "gym-buddy-v24";
 const IMG_CACHE = "gym-buddy-exercise-img-v2";
 const IMG_PATH = "/Gym-buddy/img/exercises/";
 const ASSETS = ["./", "./index.html", "./manifest.json"];
