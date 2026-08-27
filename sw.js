5784b01865d9665e34fc48a368ebf4b6b381c4a8
IsraelToledano <86730653+IsraelToledano@users.noreply.github.com>
2026-08-27 18:57:52 +0300

    v29: fix cut-line stripping main/heavy-top-set exercises when dragged below abs

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "gym-buddy-v28";
+const CACHE = "gym-buddy-v29";
 const IMG_CACHE = "gym-buddy-exercise-img-v2";
 const IMG_PATH = "/Gym-buddy/img/exercises/";
 const ASSETS = ["./", "./index.html", "./manifest.json"];
