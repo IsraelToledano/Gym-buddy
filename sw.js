4159cea64c9e08e5e788b8b4e3ee44217754ccb3
IsraelToledano <86730653+IsraelToledano@users.noreply.github.com>
2026-08-31 19:17:15 +0300

    v37: bump cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "gym-buddy-v36";
+const CACHE = "gym-buddy-v37";
 const IMG_CACHE = "gym-buddy-exercise-img-v2";
 const IMG_PATH = "/Gym-buddy/img/exercises/";
 const ASSETS = ["./", "./index.html", "./manifest.json"];
